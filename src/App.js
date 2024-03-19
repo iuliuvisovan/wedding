113d0ae5f7402880da194cac38d43ac11de95c30
iuliu Vișovan <iuliuvisovan@gmail.com>
2024-03-19 20:31:38 +0200

    Wedding boyz

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://1000logos.net/wp-content/uploads/2024/01/Ring-Emojis.png" className="App-logo" alt="logo" />
+        <p>salutare haideti la nunta cai gratis</p>
       </header>
     </div>
   );

4f765df928ce45e7544fb67673fbc2e19faa27bb
iuliu Vișovan <iuliuvisovan@gmail.com>
2024-08-26 22:48:44 +0300

    Add some heart gradients

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,4 +1,6 @@
+import { useEffect, useState } from 'react';
 import './styles.css';
+import React from 'react';
 
 // map of transforms, which then applied to images:
 const transforms = {
@@ -6,6 +8,23 @@ const transforms = {
 };
 
 export default function LandingPage() {
+  // randomly select an image and remove all transforms from it, using react
+  // state to force re-render
+  const [selectedImageIndex, setSelectedImageIndex] = useState(0);
+
+  useEffect(() => {
+    colorizeRandomImage();
+  }, []);
+
+  const colorizeRandomImage = async () => {
+    const randomIndex = Math.floor(Math.random() * 30);
+
+    setSelectedImageIndex(randomIndex);
+    const randomDuration = Math.floor(Math.random() * 3000);
+    await wait(randomDuration);
+    colorizeRandomImage();
+  };
+
   return (
     <div className="screen-landing-page">
       <div className="backdrop"></div>
@@ -18,9 +37,22 @@ export default function LandingPage() {
       </div>
       <div className="images-wrapper">
         {new Array(30).fill(null).map((_, index) => (
-          <img key={index} src={`images/landing/image${index + 1}.jpeg`} alt={`image${index + 1}`} />
+          <img
+            key={index}
+            src={`images/landing/image${index + 1}.jpeg`}
+            alt="Iuliu & Gabriela Travelling"
+            className={selectedImageIndex === index ? 'nofilter' : ''}
+          />
         ))}
       </div>
     </div>
   );
+}
+
+export function wait(waitTimeInMs) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, waitTimeInMs);
+  });
 }
--- src/components/NavHearts/index.js
+++ src/components/NavHearts/index.js
@@ -2,12 +2,15 @@ import './styles.css';
 
 export default function NavHearts() {
   return (
-    <div className="hearts-navigation">
-      {new Array(6).fill(null).map((_, index) => (
-        <div key={index} className={'heart' + (index === 0 ? ' selected' : '')}>
-          <img src="images/hearts.png" />
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="hearts-gradient"></div>
+      <div className="hearts-navigation">
+        {new Array(6).fill(null).map((_, index) => (
+          <div key={index} className={'heart' + (index === 0 ? ' selected' : '')}>
+            <img src="images/hearts.png" />
+          </div>
+        ))}
+      </div>
+    </>
   );
 }

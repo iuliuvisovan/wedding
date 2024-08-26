b286cea62330058655810fd4de488764cd584b80
iuliu Vișovan <iuliuvisovan@gmail.com>
2024-08-26 20:41:52 +0300

    Add hearts navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import LandingPage from './components/LandingPage';
 import ScreenTwo from './components/ScreenTwo';
+import NavHearts from './components/NavHearts';
 
 export default function App() {
   return (
     <div className="App">
+      <NavHearts/>
       <LandingPage />
       <ScreenTwo />
     </div>
---
+++ src/components/NavHearts/index.js
@@ -0,0 +1,13 @@
+import './styles.css';
+
+export default function NavHearts() {
+  return (
+    <div className="hearts-navigation">
+      {new Array(6).fill(null).map((_, index) => (
+        <div key={index} className={'heart' + (index === 0 ? ' selected' : '')}>
+          <img src="images/hearts.png" />
+        </div>
+      ))}
+    </div>
+  );
+}

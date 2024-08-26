0125daec27b4afe6d721c14ba9af47e5196965e2
iuliu Vișovan <iuliuvisovan@gmail.com>
2024-08-26 19:52:18 +0300

    Alright landing page more or less done

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -9,10 +9,16 @@ export default function LandingPage() {
   return (
     <div className="screen-landing-page">
       <div className="backdrop"></div>
-      <div className="title">
-        <div className="and-symbol">&</div>
-        <div className="name-word">Iuliu</div>
-        <div className="name-word">Gabriela</div>
+      <div className="title-and-subtitle">
+        <div className="title">
+          <div className="and-symbol">&</div>
+          <div className="name-word">Iuliu</div>
+          <div className="name-word">Gabriela</div>
+        </div>
+        <div className="subtitle">
+          {/* <img src={`images/calendar.svg`} /> */}
+          17<span className='gold'>.</span>05<span className='gold'>.</span>2025
+        </div>
       </div>
       <div className="images-wrapper">
         {new Array(30).fill(null).map((_, index) => (

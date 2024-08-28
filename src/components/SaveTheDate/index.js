6dd03ef81b82eed6065dc62797ecfc1421768325
iuliu Vișovan <iuliuvisovan@gmail.com>
2024-08-28 21:59:32 +0300

    Ok seems to work fine, crazy stuffs

--- src/components/SaveTheDate/index.js
+++ src/components/SaveTheDate/index.js
@@ -2,13 +2,15 @@ import { useEffect, useState } from 'react';
 import { useRef } from 'react';
 import './styles.css';
 import { WITH_ANIMATIONS } from '../../App';
-import WereGettingMarried from '../../components/WereGettingMarried';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 let playInterval;
+let shouldPauseScrollListening = false;
 
 export default function SaveTheDate() {
   const videoRef = useRef();
   const [isFlipped, setIsFlipped] = useState(false);
+  const [shouldShowAnimation, setShouldShowAnimation] = useState(false);
 
   useEffect(() => {
     videoRef.current.playbackRate = 2.2;
@@ -27,10 +29,31 @@ export default function SaveTheDate() {
 
   // add scroll listener, when it's more than 250px, set state isFlipped to true
   const handleScroll = (event) => {
+    if (shouldPauseScrollListening) {
+      return;
+    }
+
     console.log('event', event);
     console.log('window.scrollY', window.scrollY);
 
-    setIsFlipped(window.scrollY > 320);
+    const newIsFlipped = window.scrollY > 370 && window.scrollY < 500;
+    setIsFlipped(newIsFlipped);
+
+    console.log('newIsFlipped', newIsFlipped);
+
+    if (newIsFlipped && !shouldShowAnimation) {
+      shouldPauseScrollListening = true;
+      setTimeout(() => {
+        setShouldShowAnimation(true);
+        shouldPauseScrollListening = false;
+      }, 1000);
+    } else {
+      setShouldShowAnimation(false);
+    }
+
+    if (window.scrollY > 500) {
+      window.showSchedule();
+    }
   };
 
   return (
@@ -40,15 +63,30 @@ export default function SaveTheDate() {
           <video ref={videoRef} src="images/ladybug.mp4" loop muted="muted" playsInline="playsinline"></video>
         </div>
 
-        <img src={`images/save-the-date.png`} />
+        <img src={`images/save-the-date.png`} alt="Save The Date Text" />
         <h2>
           17<span className="gold">.</span>05<span className="gold">.</span>2025
         </h2>
-        {/* <img src={`images/save-the-date.png`} /> */}
       </div>
       <div className={'face were-getting-married ' + (isFlipped ? 'flipped' : '')}>
-        <WereGettingMarried />
+        <div className="were-getting-married-screen">
+          <h2>We're getting</h2>
+        </div>
       </div>
+      {shouldShowAnimation && <h1 className="married">MARRIED</h1>}
+
+      {WITH_ANIMATIONS && shouldShowAnimation && (
+        <div className="animation-wrapper">
+          <DotLottieReact
+            width="100vw"
+            src="https://assets-v2.lottiefiles.com/a/b1b163e6-1165-11ee-8de3-13c51904addb/8l4FM0Hz1c.lottie"
+            loop
+            className="fireworks"
+            autoplay
+            speed={1}
+          />
+        </div>
+      )}
     </div>
   );
 }

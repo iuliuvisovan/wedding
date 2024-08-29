6f7db0926e47abebecedaa7da206e37b00863bd2
iuliu Vișovan <iuliuvisovan@gmail.com>
2024-08-29 23:59:11 +0300

    Solved local lottie files

--- src/components/SaveTheDateCard/index.js
+++ src/components/SaveTheDateCard/index.js
@@ -101,14 +101,7 @@ export default function SaveTheDate() {
 
       {WITH_ANIMATIONS && showsConfetti && (
         <div className="animation-wrapper">
-          <DotLottieReact
-            width="100vw"
-            src="https://assets-v2.lottiefiles.com/a/b1b163e6-1165-11ee-8de3-13c51904addb/8l4FM0Hz1c.lottie"
-            loop
-            className="fireworks"
-            autoplay
-            speed={1}
-          />
+          <DotLottieReact width="100vw" src="images/fireworks.lottie" loop className="fireworks" autoplay speed={1} />
         </div>
       )}
     </div>

a5aec01b4685d49ef571b9736c0f63fc6e59b75b
iuliu Vișovan <iuliuvisovan@gmail.com>
2024-08-30 23:34:25 +0300

    Initial version godparents done

--- src/components/MeetTheGodparents/index.js
+++ src/components/MeetTheGodparents/index.js
@@ -4,8 +4,27 @@ import './styles.css';
 export default function MeetTheGodparents() {
   return (
     <div className="meet-the-godparents">
-      <h2>meet...</h2>
-      <h1>The Godparents</h1>
+      <div className="meet">meet...</div>
+      <div className="the-godparents">The Godparents</div>
+      <div className="img-wrapper">
+        <img src="images/the-godparents.jpg" />
+        <div className="names">
+          <div className="family-name">Branic</div>
+          <div className="first-names">
+            <span className="first-name">Ioana</span>
+            <span className="and-symbol">&</span>
+            <span className="first-name">Tony</span>
+          </div>
+        </div>
+        <div className="names on-top">
+          <div className="family-name">Branic</div>
+          <div className="first-names">
+            <span className="first-name">Ioana</span>
+            <span className="and-symbol">&</span>
+            <span className="first-name">Tony</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

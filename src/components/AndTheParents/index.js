9ba7d5fb5cfa0edb28270806386d9f5f8b33d26b
iuliu Vișovan <iuliuvisovan@gmail.com>
2024-08-31 12:57:05 +0300

    Parents v1.1

--- src/components/AndTheParents/index.js
+++ src/components/AndTheParents/index.js
@@ -7,25 +7,24 @@ export default function MeetTheGodparents() {
       <div className="and-our-beloved">and our beloved</div>
       <div className="parents">Parents</div>
       <div className="names">
-        <div className="family-name-wrapper">
-          <div className="family-name">Visovan</div>
-          <div className="comma">,</div>
-        </div>
         <div className="first-names">
           <span className="first-name">Marius</span>
           <span className="and-symbol">&</span>
           <span className="first-name">Magdalena</span>
         </div>
+        <div className="family-name-wrapper">
+          <div className="family-name">Visovan</div>
+          <div className="comma">,</div>
+        </div>
       </div>
       <div className="names">
-        <div className="family-name-wrapper">
-          <div className="family-name">Lucan</div>
-        </div>
-
         <div className="first-names">
           <span className="first-name">Elena</span>
           <span className="and-symbol">&</span>
           <span className="first-name">Gheorghe</span>
+        </div>
+        <div className="family-name-wrapper">
+          <div className="family-name">Lucan</div>
         </div>
       </div>
     </div>

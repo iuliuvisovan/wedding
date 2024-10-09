b28aace7939100cffc011d6730b90549caa3b0bd
iuliu Vișovan <iuliuvisovan@gmail.com>
2024-10-09 11:35:06 +0300

    Map & gparents looking good

--- src/components/MeetTheGodparents/index.js
+++ src/components/MeetTheGodparents/index.js
@@ -4,28 +4,30 @@ import './styles.css';
 export default function MeetTheGodparents() {
   return (
     <div className="meet-the-godparents">
-      <div className="meet">meet...</div>
-      <div className="the-godparents">The Godparents</div>
-      <div className="img-wrapper">
-        <div className="top-gradient"></div>
-        <img src="images/the-godparents.jpg" />
-        <div className="names">
-          <div className="family-name">Branic</div>
-          <div className="first-names">
-            <span className="first-name">Ioana</span>
-            <span className="and-symbol">&</span>
-            <span className="first-name">Tony</span>
+      <div className="content-wrapper">
+        <div className="meet">meet...</div>
+        <div className="the-godparents">The Godparents</div>
+        <div className="img-wrapper">
+          <div className="top-gradient"></div>
+          <img src="images/the-godparents.jpg" />
+          <div className="names">
+            <div className="family-name">Branic</div>
+            <div className="first-names">
+              <span className="first-name">Ioana</span>
+              <span className="and-symbol">&</span>
+              <span className="first-name">Tony</span>
+            </div>
           </div>
-        </div>
-        <div className="names on-top">
-          <div className="family-name">Branic</div>
-          <div className="first-names">
-            <span className="first-name">Ioana</span>
-            <span className="and-symbol">&</span>
-            <span className="first-name">Tony</span>
+          <div className="names on-top">
+            <div className="family-name">Branic</div>
+            <div className="first-names">
+              <span className="first-name">Ioana</span>
+              <span className="and-symbol">&</span>
+              <span className="first-name">Tony</span>
+            </div>
           </div>
+          <div className="bottom-gradient"></div>
         </div>
-        <div className="bottom-gradient"></div>
       </div>
     </div>
   );
